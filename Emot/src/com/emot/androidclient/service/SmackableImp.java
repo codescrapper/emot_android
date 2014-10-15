f0d0097cd1e03b41c6e81c1df2fe0966f2ebc284
Vishal <vishal@Vishals-MacBook-Pro.local>
2014-10-15 22:38:14 +0530

    group chat added

--- Emot/src/com/emot/androidclient/service/SmackableImp.java
+++ Emot/src/com/emot/androidclient/service/SmackableImp.java
@@ -277,6 +277,8 @@ public class SmackableImp implements Smackable {
 			mConnectingThread = null;
 		}
 	}
+	
+	
 
 	/** Non-blocking, synchronized function to connect/disconnect XMPP.
 	 * This code is called from outside and returns immediately. The actual work
@@ -564,6 +566,16 @@ public class SmackableImp implements Smackable {
 		
 	}
 	
+	private void joinUser(){
+		try {
+			mGroupChat.join("test6@emot-net");
+		} catch (XMPPException e) {
+			Log.i(TAG, "Error while joining group chat");
+			e.printStackTrace();
+		}
+		
+	}
+	
 	private Iterator discoverjoinedRooms(final String pUserName){
 		
 		Iterator<String> joinedRooms = MultiUserChat.getJoinedRooms(mXMPPConnection, pUserName+"/Smack");
@@ -584,6 +596,7 @@ public class SmackableImp implements Smackable {
 	}
 
 	private void tryToConnect(boolean create_account) throws EmotXMPPException {
+	
 		try {
 			if (mXMPPConnection.isConnected()) {
 				try {
@@ -623,6 +636,8 @@ public class SmackableImp implements Smackable {
 				}
 				Log.i(TAG, "user = "+mConfig.userName + " password = "+mConfig.password + " resource = "+mConfig.ressource);
 				mXMPPConnection.login(mConfig.userName, mConfig.password, mConfig.ressource);
+				initMUC("myroom");
+				joinUser();
 			}
 			Log.i(TAG, "Trying again 222"+create_account+" .. Connected = "+mXMPPConnection.isConnected() + " authenticatec = "+mXMPPConnection.isAuthenticated());
 			Log.d(TAG, "SM: can resume = " + mStreamHandler.isResumePossible() + " needbind=" + need_bind);
@@ -1383,5 +1398,17 @@ public class SmackableImp implements Smackable {
 			}
 		}
 
+	}
+
+	@Override
+	public void sendGroupMessage(String message) {
+		try {
+			Log.i(TAG, "Sending group message " +message);
+			mGroupChat.sendMessage(message);
+		} catch (XMPPException e) {
+			Log.i(TAG, "Error sending message on group chat");
+			e.printStackTrace();
+		}
+		
 	}
 }
